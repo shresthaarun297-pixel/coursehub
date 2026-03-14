f996d736030c15d4b6e0bd5a13a4a299991993b8
shresthaarun297-pixel <shresthaarun297@gmail.com>
2026-03-14 21:59:07 +0100

    Added programme level filter

--- search.php
+++ search.php
@@ -12,21 +12,39 @@ include 'config/database.php';
 <h1>Search Programmes</h1>
 
 <form method="GET" action="">
-    <input type="text" name="keyword" placeholder="Enter programme name" required>
+    <input type="text" name="keyword" placeholder="Enter programme name">
+
+    <select name="level">
+        <option value="">All Levels</option>
+        <option value="Undergraduate">Undergraduate</option>
+        <option value="Postgraduate">Postgraduate</option>
+    </select>
+
     <button type="submit">Search</button>
 </form>
 
 <?php
-if (isset($_GET['keyword'])) {
-    $keyword = $_GET['keyword'];
+if (isset($_GET['keyword']) || isset($_GET['level'])) {
+    $keyword = isset($_GET['keyword']) ? $_GET['keyword'] : '';
+    $level = isset($_GET['level']) ? $_GET['level'] : '';
 
-    $sql = "SELECT * FROM programmes WHERE name LIKE '%$keyword%' AND published = 1";
+    $sql = "SELECT * FROM programmes WHERE published = 1";
+
+    if (!empty($keyword)) {
+        $sql .= " AND name LIKE '%$keyword%'";
+    }
+
+    if (!empty($level)) {
+        $sql .= " AND level = '$level'";
+    }
+
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
             echo "<h3>".$row['name']."</h3>";
             echo "<p>".$row['description']."</p>";
+            echo "<p><strong>Level:</strong> ".$row['level']."</p>";
             echo "<a href='programme-details.php?id=".$row['id']."'>View Details</a><br><br>";
         }
     } else {
