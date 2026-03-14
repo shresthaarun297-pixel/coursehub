220693d60ba17429219f2a354663544ef6f9815d
shresthaarun297-pixel <shresthaarun297@gmail.com>
2026-03-14 21:55:42 +0100

    Improved module display grouped by year

--- programme-details.php
+++ programme-details.php
@@ -10,11 +10,19 @@ $programme = $result->fetch_assoc();
 echo "<h1>".$programme['name']."</h1>";
 echo "<p>".$programme['description']."</p>";
 
-$modules = $conn->query("SELECT * FROM modules WHERE programme_id = $id");
+$modules = $conn->query("SELECT * FROM modules WHERE programme_id = $id ORDER BY year");
+
+$currentYear = 0;
 
 echo "<h2>Modules</h2>";
 
 while($row = $modules->fetch_assoc()){
-    echo "<p>Year ".$row['year']." - ".$row['module_name']."</p>";
+
+    if($currentYear != $row['year']){
+        $currentYear = $row['year'];
+        echo "<h3>Year ".$currentYear."</h3>";
+    }
+
+    echo "<p>".$row['module_name']."</p>";
 }
 ?>
